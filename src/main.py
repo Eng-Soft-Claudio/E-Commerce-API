133b9a325b8442b197b13234a4baf1a55903e3fa
Eng-Soft_claudio <claudiodelimatosta@gmail.com>
2025-07-05 16:29:12 -0300

    feat: Inclusão de variável "origins" para melhor aderência ao CORS

--- src/main.py
+++ src/main.py
@@ -55,9 +55,14 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
+origins = [
+    "https://clt-ecommerce.vercel.app",  
+    "http://localhost:3000",             
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.CLIENT_URL],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
